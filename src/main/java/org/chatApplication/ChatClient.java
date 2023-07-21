fa7e3fe14d91d7c9d8f3d50a6860d606cfce9576
QuQuq <schoenenbergerq@bzz.ch>
2023-07-21 22:31:04 +0200

    improved user feedback

--- src/main/java/org/chatApplication/ChatClient.java
+++ src/main/java/org/chatApplication/ChatClient.java
@@ -11,6 +11,7 @@ import java.util.UUID;
 public class ChatClient {
     private String SERVER_HOST;
     private int SERVER_PORT;
+    private static final BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) {
         ChatClient client = new ChatClient();
@@ -25,18 +26,18 @@ public class ChatClient {
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
-            Scanner userInput = new Scanner(System.in);
-            System.out.print("Enter your username: ");
-            final String[] username = {userInput.nextLine()};
+            System.out.print("[*] Enter a username for this session: ");
+            String username = userInput.readLine();
 
-            if (username[0].isEmpty()) username[0] = "guest " + UUID.randomUUID();
-            System.out.println("You are now chatting with the username " + username[0]);
+            if (username.isEmpty()) username = "guest " + UUID.randomUUID();
+            System.out.println("[*] You are now chatting with the username " + username);
 
+            String finalUsername = username;
             Thread receiveThread = new Thread(() -> {
                 String message;
                 try {
                     while ((message = in.readLine()) != null) {
-                        if (!message.startsWith(username[0] + ": ")) {
+                        if (!message.startsWith(finalUsername + ": ")) {
                             System.out.println(message);
                         }
                     }
@@ -47,8 +48,8 @@ public class ChatClient {
             receiveThread.start();
 
             while (true) {
-                String message = userInput.nextLine();
-                out.println(username[0] + ": " + message);
+                String message = userInput.readLine();
+                out.println(username + ": " + message);
             }
 
         } catch (IOException e) {
@@ -57,14 +58,13 @@ public class ChatClient {
     }
 
     private void initialize() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the IP Address -> ");
-        SERVER_HOST = sc.nextLine();
-        System.out.print("Enter the Port -> ");
         try {
-            SERVER_PORT = Integer.parseInt(sc.nextLine());
-        } catch (NumberFormatException ex){
-            System.out.println("\u001B[31m[*] The port can only contain numbers.\u001B[0m");
+            System.out.print("[*] Enter the IP Address for the chat-server: ");
+            SERVER_HOST = userInput.readLine();
+            System.out.print("[*] Enter the port the server is listening on: ");
+            SERVER_PORT = Integer.parseInt(userInput.readLine());
+        } catch (NumberFormatException | IOException ex){
+            System.out.println("\u001B[31m[*] Make sure you use the correct format.\u001B[0m");
         }
     }
 }
