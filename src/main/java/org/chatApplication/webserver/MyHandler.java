ea46b9fda39369bdec79b42726c4b828041f4d73
masluse <manuel@familie-regli.ch>
2023-07-22 01:47:51 +0200

    test

--- src/main/java/org/chatApplication/webserver/MyHandler.java
+++ src/main/java/org/chatApplication/webserver/MyHandler.java
@@ -2,38 +2,36 @@ package org.chatApplication.webserver;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-import org.chatApplication.databaseHandler.MessageDao;
-import org.chatApplication.databaseHandler.model.Message;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
 
 public class MyHandler implements HttpHandler {
-    private final MessageDao messageDao = new MessageDao();
-    private final int MESSAGE_LIMIT = 200;
-
     @Override
     public void handle(HttpExchange http) throws IOException {
-        List<Message> messages = messageDao.getLastMessages(MESSAGE_LIMIT);
+        String filePath = "src/main/resources/template/index.html";
 
-        JSONArray messagesArray = new JSONArray();
-        for (Message message : messages) {
-            JSONObject messageObject = new JSONObject();
-            messageObject.put("user", message.getUsername());
-            messageObject.put("message", message.getText());
-            messageObject.put("timestamp", message.getTimestamp());
-            messagesArray.put(messageObject);
+        File file = new File(filePath);
+        if (file.exists()) {
+            byte[] fileContent = new byte[(int) file.length()];
+            FileInputStream fis = new FileInputStream(file);
+            fis.read(fileContent);
+            fis.close();
+
+            http.getResponseHeaders().set("Content-Type", "text/html");
+
+            http.sendResponseHeaders(200, fileContent.length);
+            OutputStream os = http.getResponseBody();
+            os.write(fileContent);
+            os.close();
+        } else {
+            String response = "404 Not Found";
+            http.sendResponseHeaders(404, response.length());
+            OutputStream os = http.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
         }
-
-        String response = messagesArray.toString();
-        http.getResponseHeaders().set("Content-Type", "application/json");
-        http.sendResponseHeaders(200, response.getBytes(StandardCharsets.UTF_8).length);
-        OutputStream os = http.getResponseBody();
-        os.write(response.getBytes(StandardCharsets.UTF_8));
-        os.close();
     }
 }
